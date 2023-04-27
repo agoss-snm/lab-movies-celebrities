6e09aa57feceab4cf22fe975eb6957e3924f5220
Agostina Romero <agostinaromontamatt@gmail.com>
2023-04-27 17:50:32 -0300

    labAndExtraSolved

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -19,6 +19,28 @@ router.get('/celebrities/create', (req, res, next) => {
     res.render('celebrities/new-celebrity')
 });
 
+
+router.get('/celebrities/:id', (req, res, next) => {
+    const { id } = req.params;
+    Celebrity.findById(id)
+      .then(theCelebritie => res.render('celebrities/celebritie-detail.hbs', { celebritie: theCelebritie }))
+      .catch(error => {
+        console.log('Error while retrieving book details: ', error);
+        next(error);
+      });
+  });
+
+  router.get('/celebrities/:id/edit', (req, res, next) => {
+    const { id } = req.params;
+   
+    Celebrity.findById(id)
+      .then(celebritieEdit => {
+        // console.log(bookToEdit);
+        res.render('celebrities/edit-celebritie.hbs', { celebritie: celebritieEdit }); // <-- add this line
+      })
+      .catch(error => next(error));
+  });
+
 //pos methods
 
 router.post('/celebrities/create', (req, res, next) => {
@@ -33,5 +55,23 @@ router.post('/celebrities/create', (req, res, next) => {
         })
         .catch(error => next(error));
 });
+
+router.post('/celebrities/:id/edit', (req, res, next) => {
+    const { id } = req.params;
+    const { name, occupation, catchPhrase} = req.body;
+   
+    Celebrity.findByIdAndUpdate(id, { name, occupation, catchPhrase}, { new: true })
+      .then(updateCelebritie => res.redirect(`/celebrities`)) // go to the details page to see the updates
+      .catch(error => next(error));
+  });
+
+  router.post('/celebrities/:id/delete', (req, res, next) => {
+    const { id } = req.params;
+   
+    Celebrity.findByIdAndDelete(id)
+      .then(() => res.redirect('/celebrities'))
+      .catch(error => next(error));
+  });
+
 
 module.exports = router;
